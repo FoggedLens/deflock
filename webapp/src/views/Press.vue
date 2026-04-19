e13e77c4cab3c4fc1d1f5fd6301d7ac0891b48be
Will Freeman <hohosanta@me.com>
2026-04-19 16:30:37 -0600

    update embed instructions

--- webapp/src/views/Press.vue
+++ webapp/src/views/Press.vue
@@ -28,13 +28,7 @@
 
     <h2>Embedding our Map</h2>
     <p>
-      You are welcome to embed our map on your website. Use the following HTML snippet:
-    </p>
-
-    <p>
-      <DFCode>
-        &lt;iframe src=&quot;https://maps.deflock.org&quot; width=&quot;100%&quot; height=&quot;600&quot; style=&quot;border: none;&quot;&gt;&lt;/iframe&gt;
-      </DFCode>
+      You are welcome to embed our map on your website. Simply <a href="https://maps.deflock.org" target="_blank">click the share button on our map</a>, and copy the embed code.
     </p>
 
     <h2>Contact Us</h2>
@@ -49,5 +43,4 @@
 <script setup lang="ts">
 import DefaultLayout from '@/layouts/DefaultLayout.vue';
 import Hero from '@/components/layout/Hero.vue';
-import DFCode from "@/components/DFCode.vue";
 </script>
