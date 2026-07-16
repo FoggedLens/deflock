4f2676adc0f378966b2db103b0b7e1db699a3d65
stopflock <admin@stopflock.com>
2026-07-15 21:10:40 -0500

    Social media links (#129)
    * fix line break rendering of store page collection descriptions
    
    * first pass test
    
    * little tweaks
    
    * reorder items, try again to fix store desc line breaks
    
    * maybe fix footer items getting cut off
    
    * third times a charm
    
    * so close.. I think this is it.
    
    * final-new-2-latest-v2... treat natural string length as min size
    
    * okay, starting over, new approach to footer section wrapping..
    
    * stop footer sections bunching when there IS room
    
    * I hate webdev
    
    * whitespace and sizing and offsets, oh my!
    
    * smoooooth resize
    
    * move store link to resources

--- webapp/src/App.vue
+++ webapp/src/App.vue
@@ -3,12 +3,13 @@ import { RouterView, useRouter } from 'vue-router'
 import { computed, ref, watch, onMounted } from 'vue'
 import { useTheme } from 'vuetify';
 import DiscordWarningDialog from '@/components/DiscordWarningDialog.vue';
+import OfficialSocials from '@/components/layout/OfficialSocials.vue';
 import { useDiscordIntercept } from '@/composables/useDiscordIntercept';
 
 const theme = useTheme();
 const router = useRouter();
-const isDark = computed(() => theme.name.value === 'dark');
 const isInIframe = computed(() => window.self !== window.top);
+
 const { showDialog, discordUrl, interceptDiscordLinks } = useDiscordIntercept();
 
 function toggleTheme() {
@@ -37,23 +38,26 @@ const items = [
   { title: 'Home', icon: 'mdi-home', to: '/' },
   { title: 'Map', icon: 'mdi-map', href: 'https://maps.deflock.org' },
   { title: 'Learn', icon: 'mdi-school', to: '/what-is-an-alpr' },
-  { title: 'Store', icon: 'mdi-shopping', to: '/store' },
+  { title: 'Donate', icon: 'mdi-cash-multiple', to: '/donate' },
+
 ]
 
-const contributeItems = [
-  { title: 'Submit Cameras', icon: 'mdi-map-marker-plus', to: '/report' },
+const resourceItems = [
+  { title: 'Store', icon: 'mdi-shopping', to: '/store', isNew: true },
+  { title: 'FAQ', icon: 'mdi-help-circle', to: '/what-is-an-alpr' },
   { title: 'Public Records', icon: 'mdi-file-document', to: '/foia' },
   { title: 'City Council', icon: 'mdi-account-voice', to: '/council' },
-  { title: 'Support Us', icon: 'mdi-cash-multiple', to: '/donate' },
 ]
 
-const metaItems = [
-  { title: 'Discord', customIcon: '/icon-discord.svg', customIconDark: '/icon-discord-white.svg', customIconGrey: '/icon-discord-grey.svg', href: 'https://discord.gg/aV7v4R3sKT'},
+const getInvolvedItems = [
+  { title: 'Submit Cameras', icon: 'mdi-map-marker-plus', to: '/report' },
   { title: 'Local Groups', icon: 'mdi-account-group', to: '/groups' },
   { title: 'Contact', icon: 'mdi-email-outline', to: '/contact' },
   { title: 'GitHub', icon: 'mdi-github', href: 'https://github.com/frillweeman/deflock'},
 ];
+
 const drawer = ref(false)
+
 
 watch(() => theme.global.name.value, (newTheme) => {
   const root = document.documentElement;
@@ -105,12 +109,22 @@ watch(() => theme.global.name.value, (newTheme) => {
               :prepend-icon="item.icon"
             >
               {{ item.title }}
+              <v-chip
+                v-if="item.isNew"
+                size="x-small"
+                color="rgb(18, 151, 195)"
+                class="ml-2 font-weight-bold"
+                label
+              >
+                New!
+              </v-chip>
             </v-btn>
+
           </div>
 
           <v-spacer></v-spacer>
 
-          <!-- Contribute section -->
+          <!-- Resources section -->
           <div class="d-flex align-center">
             <v-menu offset-y>
               <template v-slot:activator="{ props }">
@@ -120,12 +134,12 @@ watch(() => theme.global.name.value, (newTheme) => {
                   append-icon="mdi-chevron-down"
                   class="mx-1"
                 >
-                  Contribute
+                  Resources
                 </v-btn>
               </template>
               <v-list>
                 <v-list-item
-                  v-for="item in contributeItems"
+                  v-for="item in resourceItems"
                   :key="item.title"
                   :to="item.to"
                   link
@@ -133,12 +147,24 @@ watch(() => theme.global.name.value, (newTheme) => {
                   <template v-slot:prepend>
                     <v-icon>{{ item.icon }}</v-icon>
                   </template>
-                  <v-list-item-title>{{ item.title }}</v-list-item-title>
+                  <v-list-item-title>
+                    {{ item.title }}
+                    <v-chip
+                      v-if="item.isNew"
+                      size="x-small"
+                      color="rgb(18, 151, 195)"
+                      class="ml-2 font-weight-bold"
+                      label
+                    >
+                      New!
+                    </v-chip>
+                  </v-list-item-title>
                 </v-list-item>
               </v-list>
             </v-menu>
 
             <!-- Get Involved section -->
+
             <v-menu offset-y>
               <template v-slot:activator="{ props }">
                 <v-btn
@@ -152,7 +178,7 @@ watch(() => theme.global.name.value, (newTheme) => {
               </template>
               <v-list>
                 <v-list-item
-                  v-for="item in metaItems"
+                  v-for="item in getInvolvedItems"
                   :key="item.title"
                   :to="item.to"
                   :href="item.href"
@@ -160,22 +186,30 @@ watch(() => theme.global.name.value, (newTheme) => {
                   link
                 >
                   <template v-slot:prepend>
-                    <v-icon v-if="item.icon">{{ item.icon }}</v-icon>
-                    <v-img 
-                      v-else-if="item.customIcon"
-                      class="mr-8"
-                      contain 
-                      width="24" 
-                      height="24" 
-                      :src="isDark ? item.customIconDark : item.customIconGrey" 
-                    />
+                    <v-icon>{{ item.icon }}</v-icon>
                   </template>
                   <v-list-item-title>{{ item.title }}</v-list-item-title>
                 </v-list-item>
               </v-list>
             </v-menu>
+
+            <!-- Official Socials section -->
+            <v-menu offset-y>
+              <template v-slot:activator="{ props }">
+                <v-btn
+                  variant="text"
+                  v-bind="props"
+                  append-icon="mdi-chevron-down"
+                  class="mx-1"
+                >
+                  Official Socials
+                </v-btn>
+              </template>
+              <OfficialSocials />
+            </v-menu>
           </div>
         </div>
+
 
         <v-spacer class="d-md-none" />
 
@@ -203,15 +237,25 @@ watch(() => theme.global.name.value, (newTheme) => {
           >
             <v-icon start>{{ item.icon }}</v-icon>
             {{ item.title }}
+            <v-chip
+              v-if="item.isNew"
+              size="x-small"
+              color="rgb(18, 151, 195)"
+              class="ml-2 font-weight-bold"
+              label
+            >
+              New!
+            </v-chip>
           </v-list-item>
         </v-list>
 
+
         <v-divider class="my-2" aria-hidden="true" role="presentation" />
 
-        <v-list-subheader class="px-4">Contribute</v-list-subheader>
-        <v-list nav aria-label="Contribute Links">
+        <v-list-subheader class="px-4">Resources</v-list-subheader>
+        <v-list nav aria-label="Resource Links">
           <v-list-item
-            v-for="item in contributeItems"
+            v-for="item in resourceItems"
             :key="item.title"
             link
             :to="item.to"
@@ -219,15 +263,24 @@ watch(() => theme.global.name.value, (newTheme) => {
           >
             <v-icon v-if="item.icon" start>{{ item.icon }}</v-icon>
             <span style="vertical-align: middle;">{{ item.title }}</span>
+            <v-chip
+              v-if="item.isNew"
+              size="x-small"
+              color="rgb(18, 151, 195)"
+              class="ml-2 font-weight-bold"
+              label
+            >
+              New!
+            </v-chip>
           </v-list-item>
         </v-list>
           
         <v-divider class="my-2" aria-hidden="true" role="presentation" />
           
         <v-list-subheader class="px-4">Get Involved</v-list-subheader>
-        <v-list nav aria-label="Meta Links">
+        <v-list nav aria-label="Get Involved Links">
           <v-list-item
-            v-for="item in metaItems"
+            v-for="item in getInvolvedItems"
             :key="item.title"
             link
             :to="item.to"
@@ -236,19 +289,16 @@ watch(() => theme.global.name.value, (newTheme) => {
             role="option"
           >
             <v-icon v-if="item.icon" start>{{ item.icon }}</v-icon>
-            <v-img
-              v-else-if="item.customIcon"
-              class="mr-2 custom-icon"
-              contain
-              width="24"
-              height="24"
-              :src="isDark ? item.customIconDark : item.customIcon"
-              style="vertical-align: middle;"
-            />
             <span style="vertical-align: middle;">{{ item.title }}</span>
           </v-list-item>
         </v-list>
+
+        <v-divider class="my-2" aria-hidden="true" role="presentation" />
+
+        <v-list-subheader class="px-4">Official Socials</v-list-subheader>
+        <OfficialSocials />
       </v-navigation-drawer>
+
     </template>
 
     <v-main>
--- webapp/src/components/layout/Footer.vue
+++ webapp/src/components/layout/Footer.vue
@@ -1,60 +1,90 @@
 <template>
   <v-footer>
-    <v-container>
+    <v-container fluid>
+
       <v-row align-items="center" justify="center">
 
         <v-col cols="12" class="mt-4">
           <v-img height="30" contain src="/deflock-logo-grey.svg" />
         </v-col>
         
-        <!-- Internal Links -->
-        <v-col cols="7" sm="3">
-          <v-list-subheader class="mx-4 font-weight-black text-subtitle-1" :class="isDark ? 'text-grey-lighten-5' : 'text-black'" id="footer-info-heading">Info</v-list-subheader>
-          <v-list density="compact" aria-labelledby="footer-info-heading" role="list">
-            <v-list-item role="listitem"
-              v-for="link in internalLinks"
-              :key="link.title"
-              link
-              :to="link.to"
-              slim
-              :aria-label="link.alt"
-            >
-                <v-list-item-title class="d-flex align-center">
-                <v-icon class="custom-icon" start :icon="link.icon" :alt="link.alt" />
-                {{ link.title }}
-                </v-list-item-title>
-            </v-list-item>
-          </v-list>
+        <!-- Link sections: CSS grid with auto-fit/minmax so columns reflow
+             to a new row dynamically based on available width, rather than
+             snapping to a fixed breakpoint that can leave text truncated
+             (e.g. "Terms of Se…") right before it reorders. -->
+        <v-col cols="12">
+          <div class="footer-links-grid">
+
+            <!-- Info -->
+            <div>
+              <v-list-subheader class="mx-2 font-weight-black text-subtitle-1" :class="isDark ? 'text-grey-lighten-5' : 'text-black'" id="footer-info-heading">Info</v-list-subheader>
+              <v-list density="compact" aria-labelledby="footer-info-heading" role="list">
+                <v-list-item role="listitem"
+                  v-for="link in internalLinks"
+                  :key="link.title"
+                  link
+                  :to="link.to"
+                  slim
+                  :aria-label="link.alt"
+                >
+                    <v-list-item-title class="d-flex align-center">
+                    <v-icon class="custom-icon" start :icon="link.icon" :alt="link.alt" />
+                    {{ link.title }}
+                    </v-list-item-title>
+                </v-list-item>
+              </v-list>
+            </div>
+
+            <!-- Contact Us -->
+            <div>
+              <v-list-subheader class="mx-2 font-weight-black text-subtitle-1" :class="isDark ? 'text-grey-lighten-5' : 'text-black'" id="footer-contact-heading">Contact Us</v-list-subheader>
+              <v-list density="compact" aria-labelledby="footer-contact-heading" role="list">
+                <v-list-item role="listitem"
+                  v-for="link in contactLinks"
+                  :key="link.title"
+                  link
+                  :to="link.to"
+                  slim
+                  :aria-label="link.alt"
+                >
+                    <v-list-item-title class="d-flex align-center">
+                    <v-icon class="custom-icon" start :icon="link.icon" :alt="link.alt" />
+                    {{ link.title }}
+                    </v-list-item-title>
+                </v-list-item>
+              </v-list>
+            </div>
+
+            <!-- Get Involved -->
+            <div>
+              <v-list-subheader class="mx-2 font-weight-black text-subtitle-1" :class="isDark ? 'text-grey-lighten-5' : 'text-black'" id="footer-involved-heading">Get Involved</v-list-subheader>
+              <v-list density="compact" aria-labelledby="footer-involved-heading" role="list">
+                <v-list-item
+                  v-for="link in getInvolvedLinks"
+                  :key="link.title"
+                  link
+                  slim
+                  :href="link.href"
+                  :to="link.to"
+                  :target="link.href ? '_blank' : undefined"
+                  role="listitem"
+                >
+                  <v-list-item-title class="d-flex align-center justify-start">
+                    <v-icon start class="custom-icon" :icon="link.icon"></v-icon>
+                    {{ link.title }}
+                  </v-list-item-title>
+                </v-list-item>
+              </v-list>
+            </div>
+
+          </div>
         </v-col>
 
-        <!-- External Links -->
-        <v-col cols="5" sm="3">
-          <v-list-subheader class="mx-4 font-weight-black text-subtitle-1" :class="isDark ? 'text-grey-lighten-5' : 'text-black'" id="footer-involved-heading">Get Involved</v-list-subheader>
-          <v-list density="compact" aria-labelledby="footer-involved-heading" role="list">
-            <v-list-item
-              v-for="link in externalLinks"
-              :key="link.title"
-              link
-              slim
-              :href="link.href"
-              :to="link.to"
-              :target="link.href ? '_blank' : undefined"
-              role="listitem"
-            >
-              <v-list-item-title class="d-flex align-center justify-start">
-                <v-icon start v-if="link.icon" class="custom-icon" :icon="link.icon"></v-icon>
-                <img v-else-if="link.customIcon" class="mr-2 custom-icon" width="24" height="24" :src="isDark ? link.customIconDark : link.customIcon" :alt="link.alt" />
-                {{ link.title }}
-              </v-list-item-title>
-            </v-list-item>
-          </v-list>
-        </v-col>
 
         <!-- Copyright -->
         <v-col
           class="text-center d-flex align-center justify-center text-grey-darken-1"
           cols="12"
-          sm="6"
         >
           <div class="copyright" :class="isDark ? 'text-grey-lighten-5' : 'text-black'">
             <p>&copy; {{ currentYear }} DeFlock. All Rights Reserved</p>
@@ -78,14 +108,17 @@ const internalLinks = [
   { title: 'About', to: '/about', icon: 'mdi-information', alt: 'About' },
   { title: 'Privacy Policy', to: '/privacy', icon: 'mdi-shield-lock', alt: 'Privacy Policy' },
   { title: 'Terms of Service', to: '/terms', icon: 'mdi-file-document', alt: 'Terms of Service' },
-  { title: 'Press', to: '/press', icon: 'mdi-newspaper', alt: 'Press' },
 ];
 
-const externalLinks = [
-  { title: 'Discord', href: 'https://discord.gg/aV7v4R3sKT', customIcon: '/icon-discord.svg', customIconDark: '/icon-discord-white.svg', alt: 'Discord Logo' },
-  { title: 'Support Us', to: '/donate', icon: 'mdi-cash-multiple', alt: 'Support Us' },
-  { title: 'GitHub', href: 'https://github.com/FoggedLens/deflock', icon: 'mdi-github', alt: 'GitHub Logo' },
+const contactLinks = [
+  { title: 'Press', to: '/press', icon: 'mdi-newspaper', alt: 'Press' },
   { title: 'Contact', to: '/contact', icon: 'mdi-email', alt: 'Contact' },
+];
+
+const getInvolvedLinks = [
+  { title: 'Local Groups', to: '/groups', icon: 'mdi-account-group' },
+  { title: 'GitHub', href: 'https://github.com/FoggedLens/deflock', icon: 'mdi-github' },
+  { title: 'Submit Cameras', to: '/report', icon: 'mdi-map-marker-plus' },
 ]
 </script>
 
@@ -97,4 +130,46 @@ const externalLinks = [
   font-size: 0.85rem;
   line-height: 0.5rem;
 }
+
+/* Plain flexbox + wrap, rather than CSS grid's auto-fit/minmax: grid's
+   auto-fit track-counting algorithm doesn't properly support intrinsic
+   sizes (max-content) as a track minimum, which is why that approach
+   either clipped text, wrapped it mid-word, or collapsed to one column
+   depending on the minimum used. Flexbox with flex-wrap naturally sizes
+   each item to its content and only wraps an item to the next line once
+   it genuinely doesn't fit — no guessed pixel thresholds required.
+
+   Columns are NOT stretched (flex-grow: 0) — each stays sized to its own
+   content. Letting them grow to fill space (a prior revision) made wider
+   columns pad out with empty space to the right of their left-aligned
+   text, which visually dragged the whole row's "center of mass" left of
+   the row's true center — an effect that got worse the more room there
+   was to stretch into, i.e. on wider screens.
+
+   Instead, `justify-content: space-evenly` distributes the *gaps* — both
+   between columns and on the outer edges — based on the container's
+   actual width. That gap recalculates continuously as the viewport
+   resizes, so spacing scales smoothly with no fixed max-width/threshold
+   needed and no snapping between layout states. */
+.footer-links-grid {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+  row-gap: 24px;
+}
+
+.footer-links-grid > div {
+  flex: 0 0 auto;
+}
+/* Keep each link label on a single line so a column reflows to its own
+   row instead of wrapping mid-word when space is tight. */
+.footer-links-grid :deep(.v-list-item-title) {
+  white-space: nowrap;
+}
 </style>
+
+
+
+
+
+
